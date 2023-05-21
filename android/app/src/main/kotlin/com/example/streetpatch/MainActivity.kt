77b34498432640adb2f88609ad0e7d0cd5d83a11
Krishnaveni <krishnavenim2024@cs.sjcetpalai.ac.in>
2023-05-21 11:04:34 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/streetpatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.streetpatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/open_page/login/login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class loginPage extends StatelessWidget {
+  const loginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 10, 10, 10),
+      body: SafeArea(
+          child: Center(
+        child: Column(children: [
+          //streetpatch
+          SizedBox(height: 230),
+          Text(
+            'STREET PATCH',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Color.fromARGB(255, 238, 224, 228)),
+          ),
+          SizedBox(height: 10),
+          //sign up
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 114, 110, 110),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                'sign up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ]),
+      )),
+    );
+  }
+}
