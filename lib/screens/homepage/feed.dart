c01746d5f6c4a2ba82ceecb63b4a98adc993eeeb
SaNitYgRiM <85287339+SaNitYgRiM@users.noreply.github.com>
2023-05-29 22:31:22 +0530

    Revert "Revert "merged""
    This reverts commit 8a693d17d98fc8f22ade2589430860b8007d372e.

--- lib/screens/homepage/feed.dart
+++ lib/screens/homepage/feed.dart
@@ -42,10 +42,6 @@ class _homePageState extends State<homePage> {
                                         side: const BorderSide(
                                             color: Color.fromARGB(
                                                 255, 51, 54, 240))))),
-<<<<<<< HEAD
-                            onPressed: () {},
-                            child: const Text(
-=======
                             onPressed: () {
                               Navigator.of(context)
                                   .push(MaterialPageRoute(builder: (context) {
@@ -53,7 +49,6 @@ class _homePageState extends State<homePage> {
                               }));
                             },
                             child: Text(
->>>>>>> b007b755e2892d028e3799f1568a60b270e5fc24
                               'Register Complaints',
                               style: TextStyle(
                                 color: Color.fromARGB(248, 255, 255, 255),
